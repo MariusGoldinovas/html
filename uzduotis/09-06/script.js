8b4c1e320c6f88e9fe527c3dfe4d825fa47eef6d
Marius <goldinovas.marius@gmail.com>
2024-09-06 13:01:31 +0300

    update

--- uzduotis/09-06/script.js
+++ uzduotis/09-06/script.js
@@ -1,18 +1,35 @@
 let selectedBreed = '';
 
+
 fetch('https://dog.ceo/api/breeds/list/all')
   .then((response) => response.json())
   .then((data) => {
 
-    const breeds = Object.keys(data.message); 
+    const breeds = data.message; 
     const selectElement = document.querySelector('.pets');
-
-    breeds.forEach(breed => {
-      const option = document.createElement('option');
-      option.value = breed;
-      option.textContent = breed;
-      selectElement.appendChild(option);
+    let totalBreeds = 0;
+    let totalSubBreeds = 0;
+    
+    Object.keys(breeds).forEach(breed => {
+        totalBreeds++;
+        if (breeds[breed].length > 0){
+            breeds[breed].forEach(subBreed => {
+                totalSubBreeds++;
+                const option = document.createElement('option');
+                option.value = `${breed}/${subBreed}`;
+                option.textContent = `${breed} (${subBreed})`;
+                selectElement.appendChild(option);
+            });
+        } else {
+            const option = document.createElement('option');
+            option.value = breed;
+            option.textContent = breed;
+            selectElement.appendChild(option);
+        }
     });
+
+    document.querySelector('.breedNumber').innerHTML = `We have: ${totalBreeds} breeds and ${totalSubBreeds} sub-breeds.`;
+
 
     selectElement.addEventListener('change', (e) => {
         selectedBreed = e.target.value; 
