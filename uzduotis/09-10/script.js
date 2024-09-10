e7c2fb27f027ba4773e387609c311e01506e2440
Marius <goldinovas.marius@gmail.com>
2024-09-10 13:51:25 +0300

    update

--- uzduotis/09-10/script.js
+++ uzduotis/09-10/script.js
@@ -62,3 +62,24 @@ const getCocktail = (id) => {
     });
 }
 
+// // Sukuriame tuščią kintamąjį, kuris saugos sugeneruotą HTML kodą
+// let ingredientsHTML = '';
+
+// // Ciklas sukasi nuo 1 iki 15, nes API grąžina iki 15 ingredientų ir jų matavimų
+// for (let i = 1; i <= 15; i++) {
+//     // Gauname ingredientą iš objekto 'drink', kurio indeksas priklauso nuo ciklo skaičiaus (i)
+//     const ingredient = drink[`strIngredient${i}`];
+
+//     // Gauname atitinkamą ingrediento matą (pvz., 50 ml)
+//     const measure = drink[`strMeasure${i}`];
+
+//     // Jei ingredientas egzistuoja (nėra null arba undefined), tęsiame
+//     if (ingredient) {
+//         // Pridedame HTML eilutę į ingredientsHTML, kur rodoma ingrediento ir mato vertė
+//         // Jei nėra mato, naudojame tuščią vietą
+//         ingredientsHTML += `<div>${measure ? measure : ''} ${ingredient}</div>`;
+//     }
+// }
+
+// // Kai ciklas baigiasi, surandame HTML elementą su klase .ingredients ir įdedame sugeneruotą ingredientų sąrašą
+// document.querySelector('.ingredients').innerHTML = ingredientsHTML;
