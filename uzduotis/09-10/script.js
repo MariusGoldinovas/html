ce9437998bffd5d11f8bc40370223bbca0f3000a
Marius <goldinovas.marius@gmail.com>
2024-09-10 20:25:40 +0300

    update

--- uzduotis/09-10/script.js
+++ uzduotis/09-10/script.js
@@ -35,11 +35,11 @@ const getCocktail = (id) => {
                 <div class="left">
                     <div class="name">${drink.strDrink}</div>
                     <div class="instruction"><strong>Instructions:</strong> ${drink.strInstructions}</div>
-                    <div class="category"><strong>Category:</strong> ${drink.strCategory}</div>
-                    <div class="alco"><strong>Alco/NonAlco:</strong> ${drink.strAlcoholic}</div>
-                    <div class="glass"><strong>Glass type:</strong> ${drink.strGlass}</div>
-                    <div class="ingredients"><strong>Ingredients:</strong>
-                        <div class="ingredient"></div>
+                    <div class="category" onclick="category('${drink.strCategory}')"><strong>Category:</strong> ${drink.strCategory}</div>
+                    <div class="alco" onclick="alco('${drink.strAlcoholic}')"><strong>Alco/NonAlco:</strong>${drink.strAlcoholic}</div>
+                    <div class="glass" onclick="glass('${drink.strGlass}')"><strong>Glass type:</strong> ${drink.strGlass}</div>
+                    <div class="ingredients"onclick="glass('${drink.strGlass}')"><strong>Ingredients:</strong>
+                    <div class="ingredient" onclick="glass('${drink.strGlass}')"></div>
                     </div>
                 </div>
                 <div class="right">
@@ -64,70 +64,88 @@ const getCocktail = (id) => {
     });
 }
 
-// // Sukuriame tuščią kintamąjį, kuris saugos sugeneruotą HTML kodą
-// let ingredientsHTML = '';
-
-// // Ciklas sukasi nuo 1 iki 15, nes API grąžina iki 15 ingredientų ir jų matavimų
-// for (let i = 1; i <= 15; i++) {
-//     // Gauname ingredientą iš objekto 'drink', kurio indeksas priklauso nuo ciklo skaičiaus (i)
-//     const ingredient = drink[`strIngredient${i}`];
-
-//     // Gauname atitinkamą ingrediento matą (pvz., 50 ml)
-//     const measure = drink[`strMeasure${i}`];
-
-//     // Jei ingredientas egzistuoja (nėra null arba undefined), tęsiame
-//     if (ingredient) {
-//         // Pridedame HTML eilutę į ingredientsHTML, kur rodoma ingrediento ir mato vertė
-//         // Jei nėra mato, naudojame tuščią vietą
-//         ingredientsHTML += `<div>${measure ? measure : ''} ${ingredient}</div>`;
-//     }
-// }
-
-// // Kai ciklas baigiasi, surandame HTML elementą su klase .ingredients ir įdedame sugeneruotą ingredientų sąrašą
-// document.querySelector('.ingredients').innerHTML = ingredientsHTML;
 
 const back = () => {
     result.innerHTML = searchResultsHTML;
 }
 
-const alcoholicCheckbox = document.getElementById('alcoholicCheckbox');
-const nonAlcoholicCheckbox = document.getElementById('nonAlcoholicCheckbox');
-
-
-alcoholicCheckbox.addEventListener('change', filterCocktails);
-nonAlcoholicCheckbox.addEventListener('change', filterCocktails);
-
-function filterCocktails() {
-    const isAlcoholicChecked = alcoholicCheckbox.checked;
-    const isNonAlcoholicChecked = nonAlcoholicCheckbox.checked;
-
-    // If both are unchecked, clear the result
-    if (!isAlcoholicChecked && !isNonAlcoholicChecked) {
-        result.innerHTML = '<p>No filter selected.</p>';
-        return;
-    }
-    let apiUrl = '';
-
-    if (isAlcoholicChecked) {
-        apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic';
-    } else if (isNonAlcoholicChecked) {
-        apiUrl = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic';
-    }
-    // Fetch the filtered cocktails
-    fetch(apiUrl)
+function alco(alcoholType) {
+    let value = alcoholType === 'Alcoholic' ? 'Alcoholic' : 'Non_Alcoholic';
+    
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=${value}`)
     .then(resp => resp.json())
     .then(resp => {
-        // Map the drinks to HTML
-        const data = resp.drinks.map(drink => `
-            <div class="col-4 mb-4" onclick="getCocktail(${drink.idDrink})">
+        console.log(resp);
+        const data = resp.drinks.map(data => `
+            <div class="col-4 mb-4" onclick="getCocktail(${data.idDrink})">
                 <div class="image">
-                    <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}">
+                    <img src="${data.strDrinkThumb}">
                 </div>
-                <h4 class="mt-2">${drink.strDrink}</h4>
+                <h4 class="mt-2">${data.strDrink}</h4>
             </div>
         `);
-
-        // Update the result container with filtered cocktails
-        result.innerHTML = `<div class="row">${data.join('')}</div>`;
+        
+        searchResultsHTML  = `<div class="row">${data.join('')}</div>`;
+        result.innerHTML = searchResultsHTML;
+    });
+}
+function category(categoryType) {
+    let value = categoryType === 'Ordinary Drink' ? 'Ordinary_Drink' : 'Cocktail';
+    
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${value}`) 
+    .then(resp => resp.json())
+    .then(resp => {
+        console.log(resp);
+        const data = resp.drinks.map(data => `
+            <div class="col-4 mb-4" onclick="getCocktail(${data.idDrink})">
+                <div class="image">
+                    <img src="${data.strDrinkThumb}">
+                </div>
+                <h4 class="mt-2">${data.strDrink}</h4>
+            </div>
+        `);
+        
+        searchResultsHTML  = `<div class="row">${data.join('')}</div>`;
+        result.innerHTML = searchResultsHTML;
+    });
+}
+function glass(glassType) {
+    let value = glassType === 'Cocktail glass' ? 'Cocktail_glass' : 'Champagne_flute';
+    
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=${value}`) 
+    .then(resp => resp.json())
+    .then(resp => {
+        console.log(resp);
+        const data = resp.drinks.map(data => `
+            <div class="col-4 mb-4" onclick="getCocktail(${data.idDrink})">
+                <div class="image">
+                    <img src="${data.strDrinkThumb}">
+                </div>
+                <h4 class="mt-2">${data.strDrink}</h4>
+            </div>
+        `);
+        
+        searchResultsHTML  = `<div class="row">${data.join('')}</div>`;
+        result.innerHTML = searchResultsHTML;
+    });
+}
+function ingredients(ingredientsType) {
+    let value = glassType === 'Gin' ? 'Gin' : 'Vodka';
+    
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${value}`) 
+    .then(resp => resp.json())
+    .then(resp => {
+        console.log(resp);
+        const data = resp.drinks.map(data => `
+            <div class="col-4 mb-4" onclick="getCocktail(${data.idDrink})">
+                <div class="image">
+                    <img src="${data.strDrinkThumb}">
+                </div>
+                <h4 class="mt-2">${data.strDrink}</h4>
+            </div>
+        `);
+        
+        searchResultsHTML  = `<div class="row">${data.join('')}</div>`;
+        result.innerHTML = searchResultsHTML;
     });
 }
