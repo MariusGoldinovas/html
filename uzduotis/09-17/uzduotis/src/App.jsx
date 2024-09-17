2a04ea264e3804fb89b0d1a32ac7c7d236981ce8
Marius <goldinovas.marius@gmail.com>
2024-09-17 16:39:13 +0300

    skaiciuotuvas

---
+++ uzduotis/09-17/uzduotis/src/App.jsx
@@ -0,0 +1,15 @@
+import Calculator from './components/calculator/Calculator'
+import Quantity from './components/quantity/Quantity'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <Quantity />
+      <Calculator />
+    </>
+  )
+}
+
+export default App
